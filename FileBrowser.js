32e159ade37781ebab26fd75fbe6221a8953f572
command0929 <142138531+command0929@users.noreply.github.com>
2024-09-18 19:30:48 +0900

    class FileBrowser

---
+++ FileBrowser.js
@@ -0,0 +1,45 @@
+exports.FileBrowser = /** @class */(function () { // class FileBrowser
+
+    /**
+     * FileBrowser class
+     * @param {string} file_path 
+     */
+
+    function FileBrowser(file_path) {
+        this.path = file_path;
+        this.file = java.io.File(this.path);
+    }
+    /**
+     * next folder
+     * @param {string} path 
+     * @returns {boolean}
+     */
+    FileBrowser.prototype.next = function (path) {
+        let next_path = this.path + '/' + path;
+        this.file = java.io.File(next_path);
+        if(!this.file.isDirectory()) {
+            this.file = java.io.File(this.path);
+            return false;
+        }
+        this.path = next_path;
+        return true;
+    }
+    /**
+     * prev folder
+     */
+    FileBrowser.prototype.prev = function () {
+        let prev_path = this.path.split('/').slice(0, -1);
+        if(prev_path.length <= 1) prev_path = prev_path.concat(['']);
+        this.path = '/' + prev_path.join('/');
+        this.file = java.io.File(this.path);
+    }
+    /**
+     * folder + file list
+     * @returns {array}
+     */
+    FileBrowser.prototype.list = function () {
+        return this.file.list().map(str => str);
+    }
+
+    return FileBrowser;
+})();
